a781c34f0dcc09a92c62889c3d94191d9fe8a2c3
Christina Chen <cyc25>
2018-07-17 17:21:00 +0200

    build

--- dist8/src/application.js
+++ dist8/src/application.js
@@ -42,9 +42,8 @@ class BookableApiApplication extends boot_1.BootMixin(repository_1.RepositoryMix
             connector: "loopback-connector-mysql",
             host: process.env.DB_HOST,
             port: process.env.DB_PORT,
-            database: process.env.DB_DATABASE,
-            user: process.env.DB_USER,
             //database: process.env.DB_DATABASE,
+            user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
         });
         this.dataSource(dataSourceConfig);
